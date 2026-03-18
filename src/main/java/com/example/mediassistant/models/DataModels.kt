e1022c03f8327a0fd170f550a6b94f9fbc431a80
amin14c <amin14c@gmail.com>
2026-03-18 04:56:07 +0100

    Create DataModels.kt with patient, medication, and analysis data classes

---
+++ src/main/java/com/example/mediassistant/models/DataModels.kt
@@ -0,0 +1,5 @@
+data class Patient(val id: String, val name: String, val age: Int)
+
+data class Medication(val id: String, val name: String, val dosage: String)
+
+data class Analysis(val id: String, val type: String, val result: String)
